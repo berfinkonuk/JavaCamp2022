87dff11519d28e6cec64087a4d688df1a44136c0
Berfin Nur <100218840+berfinkonuk@users.noreply.github.com>
2022-10-19 01:04:39 +0300

    Add files via upload

---
+++ JavaCamp_Week3_hw1/JavaCamp_Week3_hw1/YouTubeEğitim_VisualStudio/YouTubeEğitim_VisualStudio/Program.cs
@@ -0,0 +1,102 @@
+﻿/*
+int sayi1 = 10;
+int sayi2 = 20;
+sayi1 = sayi2;
+sayi2 = 100;
+Console.WriteLine(sayi1); */
+
+/*
+int[] sayilar1 = new[] { 1, 2, 3 };
+int[] sayilar2 = new[] { 10, 20, 30 };
+sayilar1 = sayilar2;
+sayilar2[0] = 1000;
+Console.WriteLine(sayilar1[0]); */
+
+
+
+CreditManager creditManager = new CreditManager();
+creditManager.Calculate();
+creditManager.Calculate();
+creditManager.Save();
+
+Customer customer = new Customer(); //örneğini oluşturmak, instance oluşturmak, instance creation
+customer.Id = 1;
+customer.City = "Ankara";
+
+
+CustomerManager customerManager = new CustomerManager(customer);
+customerManager.Save();
+customerManager.Delete();
+
+Company company = new Company();
+company.TaxNumber = "1000000";
+company.CompanyName = "Arçelik";
+company.Id = 100;
+
+CustomerManager customerManager2 = new CustomerManager(new Person());
+
+Person person = new Person();
+person.NationalIdentity = "1234567";
+person.FirstName = "Berfin";
+
+Customer c1=new Customer();
+Customer c2=new Person();
+Customer c3=new Company();
+
+Console.ReadLine();
+
+
+class CreditManager
+{
+    public void Calculate()
+    {
+        Console.WriteLine("Hesaplandı");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Kredi verildi");
+    }
+}
+//SOLID
+class Customer
+{
+    public Customer()
+    {
+        Console.WriteLine("Müşteri nesnesi başlatıldı");
+    }
+
+    public int Id { get; set; }
+    public string City { get; set; }
+
+}
+
+class Person : Customer   //kişi müşteriden miras alır
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string NationalIdentity { get; set; }    //kişiye özel
+}
+class Company:Customer     //şirket müşteriden miras alır(inharitance)
+{
+    public string CompanyName { get; set; }    //şirkete özel
+    public string TaxNumber { get; set; } 
+}
+
+
+//Katmalı Mimariler
+class CustomerManager
+{
+    private Customer _customer;
+    public CustomerManager(Customer customer)
+    {
+        _customer = customer;
+    }
+    public void Save()
+    {
+        Console.WriteLine("Müşteri kaydedildi :" + _customer.Id);
+    }
+    public void Delete()
+    {
+        Console.WriteLine("Müşteri silindi :" + _customer.Id);
+    }
+}
---
+++ JavaCamp_Week3_hw1/JavaCamp_Week3_hw1/YouTubeEğitim_VisualStudio_2/YouTubeEğitim_VisualStudio_2/Program.cs
@@ -0,0 +1,108 @@
+﻿//Ioc Container
+CustomerManager customerManager = new CustomerManager(new Customer(),new TeacherCreditManager());
+customerManager.GiveCredit();
+
+Console.ReadLine();
+
+
+class CreditManager
+{
+    public void Calculate()
+    {
+        Console.WriteLine("Hesaplandı");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Kredi verildi");
+    }
+}
+
+interface ICreditManager
+{
+    void Calculate();
+    void Save();
+}
+
+abstract class BaseCreditManager : ICreditManager
+{
+    public abstract void Calculate();
+
+    public virtual void Save()
+    {
+        Console.WriteLine("Kaydedildi");
+    }
+}
+
+class TeacherCreditManager : BaseCreditManager, ICreditManager   //class interfacein operasyonlarını(void) doldurmak zorunda
+{
+    public override void Calculate()
+    {
+        //hesaplamalar
+        Console.WriteLine("Öğretmen kredisi hesaplandı");
+    }
+
+    public override void Save()
+    {
+        base.Save();
+    }
+}
+
+class MilitaryCreditManager : BaseCreditManager, ICreditManager
+{
+    public override void Calculate()
+    {
+        Console.WriteLine("Asker kredisi hesaplandı");
+    }
+}
+
+//SOLID
+class Customer
+{
+    public Customer()
+    {
+        Console.WriteLine("Müşteri nesnesi başlatıldı");
+    }
+
+    public int Id { get; set; }
+    public string City { get; set; }
+
+}
+
+class Person : Customer   //kişi müşteriden miras alır
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string NationalIdentity { get; set; }    //kişiye özel
+}
+class Company : Customer     //şirket müşteriden miras alır(inharitance)
+{
+    public string CompanyName { get; set; }    //şirkete özel
+    public string TaxNumber { get; set; }
+}
+
+
+//Katmalı Mimariler
+class CustomerManager
+{
+    private Customer _customer;
+    private ICreditManager _creditManager;
+    public CustomerManager(Customer customer, ICreditManager creditManager)
+    {
+        _customer = customer;
+        _creditManager = creditManager;
+    }
+    public void Save()
+    {
+        Console.WriteLine("Müşteri kaydedildi :");
+    }
+    public void Delete()
+    {
+        Console.WriteLine("Müşteri silindi :");
+    }
+
+    public void GiveCredit()
+    {
+        _creditManager.Calculate();
+        Console.WriteLine("Kredi verildi");
+    }
+}
